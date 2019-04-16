74faea05c233fd0b1a37df063da2b5e48fa72a7b
Chavi99 <44029954+Chavi99@users.noreply.github.com>
2019-04-15 20:36:39 -0700

    Update views.py

--- croppred/module/views.py
+++ croppred/module/views.py
@@ -48,11 +48,22 @@ def prediction(request):
 def roh(request):
     x=int(request.GET.get("area"))
     nm=str(request.GET.get("crop"))
-    print(nm)
     if nm == "Wheat":
         y=WheatPred(x)
-    else:
+    elif nm=="Bajra":
         y=BajraPred(x)
+    elif nm=="Chillies":
+        y=ChiliPred(x)
+    elif nm=="Groundnut":
+        y=GroundnutPred(x)
+    elif nm=="Sugarcane":
+        y=SugarcanePred(x)
+    elif nm=="Peas":
+        y=PeasPred(x)
+    elif nm=="Rice":
+        y=RicePred(x)
+    else:
+        y=JowarPred(x)
     return render(request,"production.html",{"prod":y,"crop":nm,"area":x})
 def WheatPred(x):
     print("h2")
@@ -94,4 +105,195 @@ def wheat(request):
     return render(request,"Wheat.html")
 def bajra(request):
     return render(request,"Bajra.html")
+def ChiliPred(x):
+    print("h3")
+    data=pd.read_csv('F:\crop\croppred\module\data3.csv')
+    X=data['Production'].values
+    Y=data['Area'].values
+    mean_x = np.mean(X)
+    mean_y = np.mean(Y)
+    m = len(X)
+    num = 0
+    den = 0
+    for i in range(m):
+        num += (X[i] - mean_x) * (Y[i] - mean_y)
+        den += (X[i] - mean_x) ** 2
+    b1 = num / den
+    b0 = mean_y - (b1* mean_x)
+    y=b1*x
+    return y
+def SugarcanePred(x):
+    print("h2")
+    data=pd.read_csv('F:\crop\croppred\module\data8.csv')
+    X=data['Production'].values
+    Y=data['Area'].values
+    mean_x = np.mean(X)
+    mean_y = np.mean(Y)
+    m = len(X)
+    num = 0
+    den = 0
+    for i in range(m):
+        num += (X[i] - mean_x) * (Y[i] - mean_y)
+        den += (X[i] - mean_x) ** 2
+    b1 = num / den
+    b0 = mean_y - (b1* mean_x)
+    y=b1*x
+    return y
+def PeasPred(x):
+    data=pd.read_csv('F:\crop\croppred\module\data7.csv')
+    X=data['Production'].values
+    Y=data['Area'].values
+    mean_x = np.mean(X)
+    mean_y = np.mean(Y)
+    m = len(X)
+    num = 0
+    den = 0
+    for i in range(m):
+        num += (X[i] - mean_x) * (Y[i] - mean_y)
+        den += (X[i] - mean_x) ** 2
+    b1 = num / den
+    b0 = mean_y - (b1* mean_x)
+    y=b1*x
+    return y
+def GroundnutPred(x):
+    print("h2")
+    data=pd.read_csv('F:\crop\croppred\module\data6.csv')
+    X=data['Production'].values
+    Y=data['Area'].values
+    mean_x = np.mean(X)
+    mean_y = np.mean(Y)
+    m = len(X)
+    num = 0
+    den = 0
+    for i in range(m):
+        num += (X[i] - mean_x) * (Y[i] - mean_y)
+        den += (X[i] - mean_x) ** 2
+    b1 = num / den
+    b0 = mean_y - (b1* mean_x)
+    y=b1*x
+    return y
+def JowarPred(x):
+    print("h2")
+    data=pd.read_csv('F:\crop\croppred\module\data5.csv')
+    X=data['Production'].values
+    Y=data['Area'].values
+    mean_x = np.mean(X)
+    mean_y = np.mean(Y)
+    m = len(X)
+    num = 0
+    den = 0
+    for i in range(m):
+        num += (X[i] - mean_x) * (Y[i] - mean_y)
+        den += (X[i] - mean_x) ** 2
+    b1 = num / den
+    b0 = mean_y - (b1* mean_x)
+    y=b1*x
+    return y
+def RicePred(x):
+    data=pd.read_csv('F:\crop\croppred\module\data4.csv')
+    X=data['Production'].values
+    Y=data['Area'].values
+    mean_x = np.mean(X)
+    mean_y = np.mean(Y)
+    m = len(X)
+    num = 0
+    den = 0
+    for i in range(m):
+        num += (X[i] - mean_x) * (Y[i] - mean_y)
+        den += (X[i] - mean_x) ** 2
+    b1 = num / den
+    b0 = mean_y - (b1* mean_x)
+    y=b1*x
+    return y
+def info(request):
+    return render(request,"info.html")
+def wheat(request):
+    return render(request,"Wheat.html")
+def bajra(request):
+    return render(request,"Bajra.html")
+def DeoriaWheat(x,yr):
+    data=pd.read_csv('F:\crop\croppred\module\DeoriaWheat.csv')
+    X=data['Crop_Year'].values
+    Y=data['P/A'].values
+    mean_x = np.mean(X)
+    mean_y = np.mean(Y)
+    m = len(X)
+    num = 0
+    den = 0
+    for i in range(m):
+        num += (X[i] - mean_x) * (Y[i] - mean_y)
+        den += (X[i] - mean_x) ** 2
+    b1 = num / den
+    b0 = mean_y - (b1* mean_x)
+    y=b1*yr+b0
+    y=y*x
+    return y
+def DeoriaBajra(x,yr):
+    data=pd.read_csv('F:\crop\croppred\module\DeoriaBajra.csv')
+    X=data['Crop_Year'].values
+    Y=data['P/A'].values
+    mean_x = np.mean(X)
+    mean_y = np.mean(Y)
+    m = len(X)
+    num = 0
+    den = 0
+    for i in range(m):
+        num += (X[i] - mean_x) * (Y[i] - mean_y)
+        den += (X[i] - mean_x) ** 2
+    b1 = num / den
+    b0 = mean_y - (b1* mean_x)
+    y=b1*yr+b0
+    y=y*x
+    return y
+def HathrasWheat(x,yr):
+    data=pd.read_csv('F:\crop\croppred\module\HathrasWheat.csv')
+    X=data['Crop_Year'].values
+    Y=data['P/A'].values
+    mean_x = np.mean(X)
+    mean_y = np.mean(Y)
+    m = len(X)
+    num = 0
+    den = 0
+    for i in range(m):
+        num += (X[i] - mean_x) * (Y[i] - mean_y)
+        den += (X[i] - mean_x) ** 2
+    b1 = num / den
+    b0 = mean_y - (b1* mean_x)
+    y=b1*yr+b0
+    y=y*x
+    return y
+def HathrasBajra(x,yr):
+    data=pd.read_csv('F:\crop\croppred\module\HathrasBajra.csv')
+    X=data['Crop_Year'].values
+    Y=data['P/A'].values
+    mean_x = np.mean(X)
+    mean_y = np.mean(Y)
+    m = len(X)
+    num = 0
+    den = 0
+    for i in range(m):
+        num += (X[i] - mean_x) * (Y[i] - mean_y)
+        den += (X[i] - mean_x) ** 2
+    b1 = num / den
+    b0 = mean_y - (b1* mean_x)
+    y=b1*yr+b0
+    y=y*x
+    return y
+def Shah(request):
+    return render(request,"Shah.html")
+def soh(request):
+    x=int(request.GET.get("area"))
+    nm=str(request.GET.get("crop"))
+    dist=str(request.GET.get("dist"))
+    yr=int(request.GET.get("yr"))
+    if dist=='Deoria' and nm=='Wheat':
+        y=DeoriaWheat(x,yr-1996)
+    if dist=='Deoria' and nm=='Bajra':
+        y=DeoriaBajra(x,(yr-1996)/10)
+    if dist=='Hathras' and nm=='Wheat':
+        y=HathrasWheat(x,(yr-1996)/10)
+    if dist=='Hathras' and nm=='Bajra':
+        y=HathrasBajra(x,(yr-1996)/10)
+    return render(request,"production2.html",{"prod":y,"crop":nm,"area":x,"year":yr,"dist":dist})
+
 
